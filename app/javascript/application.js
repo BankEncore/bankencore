2e62f1a5a69242456eb39609b47376b16ef62571
Thomas Sycko-Miller <tsycko@tswarren.com>
2025-09-29 21:11:50 +0000

    Final revisions for new identifiers schema

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,7 @@
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", () => {
+  const err = document.querySelector(".text-error")
+  if (err) err.scrollIntoView({ behavior: "smooth", block: "center" })
+})
